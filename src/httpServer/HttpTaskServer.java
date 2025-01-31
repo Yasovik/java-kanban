589f6770d523362d50c56a522acf93ec61ed82ef
Alexandr Zakurdaev <azakyrdaev@gmail.com>
2025-01-31 13:34:51 +0300

    KanBan v3.1 Финальное задание 9го спринта: -Добавлен сервер -Добавлены ендпоинты для манипуляций с задачами -Добавлены юнит тесты на работу с сервером

--- src/httpServer/HttpTaskServer.java
+++ src/httpServer/HttpTaskServer.java
@@ -3,8 +3,6 @@ package httpServer;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpServer;
-import httpServer.adapter.DurationAdapter;
-import httpServer.adapter.LocalDateTimeAdapter;
 import httpServer.handler.*;
 import manager.Managers;
 import manager.TaskManager;
--- src/httpServer/LocalDateTimeAdapter.java
+++ src/httpServer/LocalDateTimeAdapter.java
@@ -1,4 +1,4 @@
-package httpServer.adapter;
+package httpServer;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
